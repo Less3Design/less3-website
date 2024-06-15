ae1543649cec9d4bb8647844074b7065d32744e5
Sherbb <alansherba@gmail.com>
2024-06-15 17:17:46 -0400

    wip

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import BlockButton from "./Components/BlockButton";
+import { ReactComponent as Heart } from "./Less3Heart.svg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          YOOOOOOOOOOOOOOOO
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="m-8 rounded-[32px] bg-neutral-800 absolute left-0 right-0 top-0 bottom-0 overflow-clip animate-containerEnter">
+        {/* Top Left*/}
+        <img
+          src="bg_temp.png"
+          className="w-full h-full shrink-0 object-cover"
+        ></img>
+        <div className="absolute left-16 top-16 gap-4 grid">
+          <BlockButton active={true} color="white">
+            SCENE_COMMENTS
+          </BlockButton>
+          <BlockButton active={false} color="white">
+            FORCE_GRAPH
+          </BlockButton>
+          <BlockButton active={false} color="white">
+            CONTROL_CENTER
+          </BlockButton>
+          <BlockButton active={false} color="white">
+            OTHER_THING
+          </BlockButton>
+        </div>
+        {/* Bottom Left*/}
+        <div className="absolute left-16 bottom-16 px-9 py-6 bg-black text-white font-bold text-8xl select-none">
+          LESS_3
+        </div>
+        {/* Bottom Right*/}
+        <div className="absolute right-16 bottom-16 gap-4 grid">
+          <BlockButton active={false} color="orange" isLink={true}>
+            DOCMENTATION
+          </BlockButton>
+          <BlockButton active={false} color="blue" isLink={true}>
+            ASSET STORE
+          </BlockButton>
+        </div>
+        <div className="absolute inset-0 animate-blurFade pointer-events-none"></div>
+        {/* black fill animation*/}
+        <div className="absolute left-[-4px] right-[-4px] bottom-[-4px] top-[-4px] opacity-0 bg-black animate-containerBlack pointer-events-none"></div>
+        {/* <Heart className="absolute left-1/2 top-1/2 translate-x-[-50%] translate-y-[-50%] h-[128px] opacity-0 origin-center w-fit animate-heartEnter"></Heart>*/}
+      <div className="absolute inset-0 text-white flex items-center justify-center">
+        <div>LESS_3</div>
+      </div>
+        <div className="absolute left-[-4px] right-[-4px] bottom-[-4px] top-[-4px] opacity-0 bg-white animate-whiteFlash delay-1000 pointer-events-none"></div>
+        <div className="absolute inset-0 border-4 rounded-[36px] border-dotted border-white/40 opacity-0 drop-shadow-[0_0_8px_rgba(255,255,255,.5)] animate-containerWhite pointer-events-none"></div>
+      </div>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,45 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      animation: {
+        containerEnter: "enter 1.35s cubic-bezier(0.87, 0, 0.13, 1)",
+        blurFade: "blurFade 1.5s cubic-bezier(0.87, 0, 0.13, 1)",
+        containerWhite: "enterWhite .6s ease-out",
+        containerBlack: "enterBlack .65s cubic-bezier(0.87, 0, 0.13, 1)",
+        heartEnter: "heartEnter 0.8s cubic-bezier(0.87, 0, 0.13, 1)",
+        whiteFlash: "whiteFlash 0.4s .4s ease-in",
+      },
+      keyframes: {
+        enterWhite: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "100" },
+        },
+        whiteFlash: {
+          "0%": { opacity: "0" },
+          "50%": { opacity: "100" },
+          "100%": { opacity: "0" },
+        },
+        enterBlack: {
+          "0%": { opacity: "100" },
+          "80%": { opacity: "100" },
+          "100%": { opacity: "0" },
+        },
+        enter: {
+          "0%": { transform: "scale(0.8,0.8)" },
+          "100%": { transform: "scale(1,1)" },
+        },
+        heartEnter: {
+          "0%": { opacity: "100", transform: "scale(1,1) translate(-50%,-50%)" },
+          "100%": { opacity: "0", transform: "scale(0.5,0.5) translate(-50%,-50%)" },
+        },
+        blurFade: {
+          "0%": { backdropFilter: "blur(32px)" },
+          "100%": { backdropFilter: "blur(0px)" },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
