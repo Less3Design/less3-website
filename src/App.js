3aa263664c664ea008097c86dec2d19b350d6143
Sherbb <alansherba@gmail.com>
2024-09-22 11:27:38 -0400

    wip. Site currently broken. Router not implemented right probably

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import BlockButton from "./Components/BlockButton";
 import { ReactComponent as Heart } from "./Less3Heart.svg";
 
 function App() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
   return (
     <div className="App">
-      <div className="m-8 rounded-[32px] bg-neutral-800 absolute left-0 right-0 top-0 bottom-0 overflow-clip animate-containerEnter">
+      <div className="m-8 rounded-[32px] bg-black absolute left-0 right-0 top-0 bottom-0 overflow-clip animate-containerEnter">
         {/* Top Left*/}
         <img
-          src="bg_temp.png"
-          className="w-full h-full shrink-0 object-cover"
+          src="homepage1.png"
+          className="w-full h-full shrink-0 object-cover opacity-80 blur-[4px] hover:blur-0 hover:opacity-100 transition-all duration-1000 ease-in-out"
         ></img>
         <div className="absolute left-16 top-16 gap-4 grid">
           <BlockButton active={true} color="white">
+            ABOUT
+          </BlockButton>
+          <div className="h-1" />
+          <BlockButton active={false} color="white">
             SCENE_COMMENTS
           </BlockButton>
           <BlockButton active={false} color="white">
@@ -28,7 +34,7 @@ function App() {
 
         {/* Bottom Left*/}
         <div className="absolute left-16 bottom-16 px-9 py-6 bg-black text-white font-bold text-8xl select-none">
-          LESS_3
+          LESS 3
         </div>
         {/* Bottom Right*/}
         <div className="absolute right-16 bottom-16 gap-4 grid justify-items-end">
@@ -40,7 +46,7 @@ function App() {
           </BlockButton>
         </div>
 
-        {/* Temp under cunstruction*/}
+        {/* Temp under cunstruction
         <div className="absolute inset-0 text-white flex items-center justify-center pointer-events-none gap-0">
           <div className="flex gap-2 bg-[#FBFF33]/30 backdrop-blur-3xl items-center justify-center px-0 py-2">
             <Heart className="h-4 fill-[#FBFF33]" />
@@ -50,6 +56,7 @@ function App() {
             <Heart className="h-4 fill-[#FBFF33]" />
           </div>
         </div>
+              */}
 
         {/* Enter animation stuff below */}
         <div className="absolute inset-0 animate-blurFade pointer-events-none"></div>
@@ -58,7 +65,7 @@ function App() {
         {/* <Heart className="absolute left-1/2 top-1/2 translate-x-[-50%] translate-y-[-50%] h-[128px] opacity-0 origin-center w-fit animate-heartEnter"></Heart>*/}
         <div className="absolute inset-0 text-white flex items-center opacity-0 justify-center pointer-events-none animate-hideAfter gap-2">
           <Heart className="h-8 fill-white" />
-          <div className="font-bold text-4xl">LESS_3</div>
+          <div className="font-bold text-4xl">LESS 3</div>
         </div>
         <div className="absolute left-[-4px] right-[-4px] bottom-[-4px] top-[-4px] opacity-0 bg-white animate-whiteFlash delay-1000 pointer-events-none"></div>
         <div className="absolute inset-0 border-4 rounded-[36px] border-dotted border-white/40 opacity-0 drop-shadow-[0_0_8px_rgba(255,255,255,.5)] animate-containerWhite pointer-events-none"></div>
